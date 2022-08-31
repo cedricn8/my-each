40da15bf29f7dde2e939a5e0bcb5d9a641f65750
cedricn8 <cedcn-9mail@yahoo.com>
2022-08-30 21:35:20 -0700

    commit tree 850d141ed58ba1664c9f6adc5d0ca0f7634994e6

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,22 @@
-def my_each # put argument(s) here
+def my_each(collection)
   # code here
+  if block_given?
+    i = 0
+
+    while i < collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+
+    collection
+  else
+    "No block given."
+  end
+end
+
+collection = [1, 2, 3, 4]
+
+my_each(collection) do |i|
+  i
+  
 end
